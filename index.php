cfab8c668d36e8ab4df5187e96be6821da63ad85
Cesar Zepeda <cesarzepeda42@gmail.com>
2021-06-25 19:27:29 -0500

    Commit inicial

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="fonts.css">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="estilo_footer.css">
+</head>
+<body>
+
+<div class="imagen">
+            <img src="logo1.png" alt="Editra's logo">
+        </div>
+
+        <button id="open" class="icon-user">
+            Login
+      </button>
+      
+      <div id="modal_container" class="modal-container">
+        <div class="modal">
+          <h1>Login</h1>
+          <form action="login.php" method="POST" class="form_login">
+            <input type="text" name="usuario" id="usuario" placeholder="Usuario" required><br>
+            <input type="text" name="contraseña" id="contraseña" class="contraseña" placeholder="Contraseña" required><br>
+            <input type="submit" name="Enviar" class="btn-enviar">
+          </form>
+          <button id="close">Cerrar</button>
+        </div>
+      </div>    
+
+<header>
+    <div class="menu_bar">
+        <a href="#" class="bt-menu"><span class="icon-menu"></span></a>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#"><span class="icon-home"></span> Inicio</a></li>
+            <li><a href="#"><span class="icon-file-text"></span>Quiénes somos</a></li>
+            <li><a href="#"><span class="icon-file-text"></span>Qué hacemos</a></li>
+            <li><a href="#"><span class="icon-profile"></span>Generar cita</a></li>
+            <li><a href="#"><span class="icon-newspaper"></span>Testimoniales</a></li>
+        </ul>
+    </nav>
+</header>
+
+    <section>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque recusandae, repudiandae a delectus fugit nostrum cupiditate, quidem omnis voluptates, ipsum placeat eum odio distinctio maiores deleniti. Harum sunt culpa architecto.</p>
+
+    </section>
+
+    <footer>
+        <div class="footer-content">
+            <ul class="socials">
+                <li><a href="#"><i class="icon-facebook2"></i></a></li>
+                <li><a href="#"><i class="icon-youtube"></i></a></li>
+                <li><a href="#"><i class="icon-instagram"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+        <ul>
+            <li><a href="">Política de privacidad</a></li>
+            <li><a href="">Contáctanos</a></li>
+        </ul>
+        </div>
+    </footer>
+
+        
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="menu.js"></script>
+    <script src="popup.js"></script>
+</body>
+
+</html>
